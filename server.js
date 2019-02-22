da19aa25a8c4e6d0a6d6b71711120c4fc2162cb5
Shivam Kharga <shivam.kharga@hotmail.com>
2019-02-22 00:39:57 -0600

    modified server.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const 	express 	= require('express'),
-		mysql 		= require('mysql'),
-		db 			= require('./config/connection'),
-		bodyParser	= require('body-parser'),
-		passport	= require('passport');
+const express = require("express"),
+  mysql = require("mysql"),
+  db = require("./config/connection"),
+  bodyParser = require("body-parser"),
+  passport = require("passport");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
 
 const port = 9000;
 
-express.static('static');
+express.static("static");
 
-app.get('/', function(req, res) {
-	res.send("hello from expressjs")
+app.get("/", function(req, res) {
+  res.send("hello from expressjs");
 });
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
 
 app.listen(port, "0.0.0.0");
-console.log('listening to port' + port );
+console.log("listening to port: " + port);
