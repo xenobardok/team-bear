04f7a4d54ae7aaf0b388960d1ca405c079adefa6
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-02-23 13:14:27 -0600

    added components to work on and images folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,21 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-
-import logo from "./logo.svg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIgloo } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+
+import NavBar from "./components/layouts/NavBar";
+
+library.add(faIgloo);
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+          <NavBar />
         </Router>
       </Provider>
     );
