ab03ebe0d9ca3103feb27ee68795fc6179220f5a
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-02-24 14:03:57 -0600

    Verification for login page done

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import classnames from "classnames";
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: e.target.email.defaultValue,
+      password: e.target.password.defaultValue
+    };
+
+    // console.log(user);
+    axios
+      .post("/api/users/login", user)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+  }
   render() {
+    const { errors } = this.state;
     return (
       <Container>
-        <Form method="POST" action="/">
+        <Form onSubmit={this.onSubmit}>
           <div className="text-center mt-4 mb-4">
             <h2>Login</h2>
             <p>Enter your email address and password</p>
@@ -16,8 +52,13 @@ class Login extends Component {
               type="email"
               placeholder="Enter email"
               name="email"
-              required
+              value={this.state.email}
+              onChange={this.onChange}
+              className={classnames("", { "is-invalid": errors.email })}
             />
+            <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group controlId="formGroupPassword">
             <Form.Label>Password</Form.Label>
@@ -25,8 +66,13 @@ class Login extends Component {
               type="password"
               placeholder="Password"
               name="password"
-              required
+              value={this.state.password}
+              onChange={this.onChange}
+              className={classnames("", { "is-invalid": errors.password })}
             />
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,57 @@
 import React, { Component } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newEmail = {
+      email: this.state.email
+    };
+
+    // console.log(newEmail);
+
+    axios
+      .post("/api/users/register", newEmail)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <Container>
-        <Form method="POST" action="/">
+        <Form onSubmit={this.onSubmit}>
           <div className="text-center mt-4 mb-4">
             <h2>Register</h2>
             <p>Enter your email address to begin</p>
           </div>
           <Form.Group controlId="formGroupEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Control
+              type="email"
+              placeholder="Enter email (@ulm.edu)"
+              value={this.state.email}
+              name="email"
+              onChange={this.onChange}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
