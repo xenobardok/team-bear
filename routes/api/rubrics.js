d8eaf2f51ccf72a3e0ccf019d7e748e2108f20ed
Shivam Kharga <shivam.kharga@hotmail.com>
2019-03-05 11:20:12 -0600

    fixed error in rubric

--- routes/api/rubrics.js
+++ routes/api/rubrics.js
@@ -59,20 +59,9 @@ router.post(
 
     const email = db.escape(req.user.email);
     const type = req.user.type;
-<<<<<<< HEAD
     const dept = db.escape(req.user.dept);
-    if (req.body.Rubric_Name)
-      rubricFields.name = db.escape(req.body.Rubric_Name);
-    if (req.body.Rows_Num) rubricFields.Rows_Num = req.body.Rows_Num;
-    if (req.body.Column_Num) rubricFields.Column_Num = req.body.Column_Num;
-    if (req.body.Scale) {
-      rubricFields.Scale = req.body.Scale;
-      rubricFields.ScaleSize = db.escape(req.body.Scale.length);
-    }
-=======
-    const dept = db.escape(req.user.Dept_ID);
->>>>>>> 0d54c340f8554400480c2bf5b99bc851f9e64248
 
+    //console.log(req.user);
     if (type == "Admin") {
       const { errors, isValid } = validateRubricInput(req.body);
 
@@ -88,22 +77,15 @@ router.post(
         rubricFields.Scale = req.body.Scale;
         rubricFields.ScaleSize = db.escape(req.body.Scale.length);
       }
+
       let sql =
         "SELECT Rubric_ID FROM RUBRIC WHERE Dept_ID =" +
         dept +
-        "AND Rubric_Name=" +
+        " AND Rubric_Name=" +
         rubricFields.name;
 
       db.query(sql, (err, result) => {
-<<<<<<< HEAD
-        if (err)
-          // return res
-          //   .status(400)
-          //   .json({ message: "Rubric already exists with that name" });
-          return err;
-=======
         if (err) throw err;
->>>>>>> 0d54c340f8554400480c2bf5b99bc851f9e64248
         else {
           if (result.length > 0) {
             errors.Rubric_Name = "Rubric with that name already exists.";
@@ -121,7 +103,7 @@ router.post(
             "," +
             dept +
             ")";
-          // console.log(sql);
+          //console.log(sql);
 
           // CREATE RUBRIC_ROW TABLE WITH Rows_Num and Column_Num
           db.query(sql, (err, result) => {
