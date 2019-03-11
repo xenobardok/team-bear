8d44b025f40ef33016de41051479152fdc0439e8
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-03-11 18:50:10 -0500

    devtools for redux reduced from production

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,17 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
-
+const devTools =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
