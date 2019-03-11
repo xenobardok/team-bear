913cb5cda5bc6d5bd07cbce6f1a3b14a7a9f6bf4
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-03-11 18:14:47 -0500

    fixed typo in secretjs

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = { secret: process.env.SECRET };
+  module.exports = { secretOrKey: process.env.SECRET };
 } else {
   module.exports = require("./secret_dev");
 }
