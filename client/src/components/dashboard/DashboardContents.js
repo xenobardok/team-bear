19199a25dd0bfcb6c3719cbc885e0e49fefc1393
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-03-27 15:46:20 -0500

    Update DashboardContents.js

--- client/src/components/dashboard/DashboardContents.js
+++ client/src/components/dashboard/DashboardContents.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import Spinner from "../../common/Spinner";
-import { Switch, Route } from "react-router";
+import { Route } from "react-router-dom";
+import {Switch} from "react-router";
 // import PropTypes from "prop-types";
 // import { connect } from "react-redux";
 // import { getCycles } from "../../actions/cycleActions";
