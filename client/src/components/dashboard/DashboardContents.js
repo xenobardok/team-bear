08d027bbc78269ce1091896842d24e45292b2214
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-03-28 17:38:50 -0500

    react-router-dom to react-router in dashboard contents

--- client/src/components/dashboard/DashboardContents.js
+++ client/src/components/dashboard/DashboardContents.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import Spinner from "../../common/Spinner";
 import { Route } from "react-router-dom";
-import {Switch} from "react-router";
+import { Switch } from "react-router";
 // import PropTypes from "prop-types";
 // import { connect } from "react-redux";
 // import { getCycles } from "../../actions/cycleActions";
