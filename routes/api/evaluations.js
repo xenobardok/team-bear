8a5e37bd0224e7049128f269f46444ee7c29f4ca
Shivam Kharga <shivam.kharga@hotmail.com>
2019-03-31 19:02:25 -0500

    fixed evaluations get

--- routes/api/evaluations.js
+++ routes/api/evaluations.js
@@ -17,26 +17,25 @@ router.get(
   "/rubrics",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    console.log("Here");
     const email = db.escape(req.user.email);
     const type = req.user.type;
     const dept = db.escape(req.user.dept);
 
     Rubrics = [];
     let sql =
-      "SELECT * FROM RUBRIC_MEASURE_EVALUATOR NATURAL JOIN RUBRIC_MEASURES NATURAL JOIN RUBRIC WHERE Evaluator_Email =" +
+      "SELECT Rubric_Measure_ID, CONCAT(Class_Name,'  ',Rubric_Name) AS Rubric_Name  FROM RUBRIC_MEASURE_EVALUATOR NATURAL JOIN RUBRIC_MEASURES NATURAL JOIN RUBRIC WHERE Evaluator_Email =" +
       email;
-    console.log(sql);
+
     db.query(sql, (err, result) => {
       if (err)
         res.status(404).json({ error: "There was a problem loading it" });
       else {
         result.forEach(row => {
-          id = row.Rubric_ID;
+          id = row.Rubric_Measure_ID;
           name = row.Rubric_Name;
 
           rubric = {
-            Rubric_ID: id,
+            Rubric_Measure_ID: id,
             Rubric_Name: name
           };
           Rubrics.push(rubric);
@@ -47,24 +46,24 @@ router.get(
   }
 );
 
-// @route   GET api/evaluations/rubrics/:RubricID
-// @desc    Returns the list of all the assigned rubrics
+// @route   GET api/evaluations/rubrics/:RubricMeasureID
+// @desc    Returns the details about an assigned Rubric from the Rubric_Measure_ID
 // @access  Private route
 router.get(
-  "/rubrics/:RubricID",
+  "/rubrics/:RubricMeasureID",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const email = db.escape(req.user.email);
     const type = req.user.type;
     const dept = db.escape(req.user.dept);
-    const Rubric_ID = req.params.RubricID;
+    const Rubric_Measure_ID = req.params.RubricMeasureID;
     const Rubric = {};
 
     let sql =
       "SELECT * FROM RUBRIC_MEASURE_EVALUATOR NATURAL JOIN RUBRIC_MEASURES NATURAL JOIN RUBRIC WHERE Evaluator_Email =" +
       email +
-      " AND Rubric_ID =" +
-      Rubric_ID;
+      " AND Rubric_Measure_ID =" +
+      Rubric_Measure_ID;
 
     console.log(sql);
     db.query(sql, (err, result) => {
@@ -74,7 +73,7 @@ router.get(
         if (result.length < 1) {
           return res.status(404).json({ error: "Rubric Not Found" });
         }
-        Rubric.Rubric_ID = Rubric_ID;
+        Rubric.Rubric_ID = result[0].Rubric_ID;
         Rubric.Rubric_Name = result[0].Rubric_Name;
         Rubric.Rows_Num = result[0].Rows_Num;
         Rubric.Column_Num = result[0].Column_Num;
@@ -84,7 +83,7 @@ router.get(
 
         sql =
           "SELECT * FROM RUBRIC NATURAL JOIN RUBRIC_SCALE WHERE Rubric_ID =" +
-          Rubric_ID +
+          Rubric.Rubric_ID +
           " ORDER BY Value ASC";
 
         db.query(sql, (err, result) => {
@@ -100,7 +99,7 @@ router.get(
 
             sql =
               "SELECT * FROM RUBRIC_STUDENTS NATURAL JOIN RUBRIC_MEASURES WHERE Rubric_ID=" +
-              Rubric_ID;
+              Rubric.Rubric_ID;
 
             db.query(sql, (err, result) => {
               if (err) return res.status(400).json(err);
@@ -177,7 +176,7 @@ router.get(
 );
 
 // @route   GET api/evaluations/rubrics/:RubricID/student/:studentID
-// @desc    Returns the list of all the assigned rubrics
+// @desc    Returns the grades of the student
 // @access  Private route
 router.get(
   "/rubrics/:RubricID/student/:studentID",
@@ -215,154 +214,19 @@ router.get(
   }
 );
 
-// @route   POST api/evaluations/rubrics/:rubricID/student/:studentID/rubricsRow/:RubricRowID
-// @desc    Adds a grade to a student for a particular grade
+// @route   POST api/evaluations/rubrics/:rubricID/student/:studentID
+// @desc    Adds a grade to a student for a particular rubric
 // @access  Private route
 router.post(
-  "/rubrics/:rubricID/student/:studentID/rubricRow/:RubricRowID",
+  "/rubricMeasure/:rubricMeasureID/student/:studentID",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const email = db.escape(req.user.email);
     const type = req.user.type;
     const dept = db.escape(req.user.dept);
     const score = req.body.Score;
-    const Rubric_ID = req.params.rubricID;
-    const Rubric_Row_ID = req.params.RubricRowID;
+    const Rubric_Measure_ID = db.escape(req.params.rubricMeasureID);
     const Student_ID = db.escape(req.params.studentID);
-
-    let sql =
-      "SELECT * FROM RUBRIC_ROW NATURAL JOIN RUBRIC_STUDENTS WHERE Rubric_Row_ID = " +
-      Rubric_Row_ID +
-      " AND Student_ID=" +
-      Student_ID +
-      " AND Rubric_ID=" +
-      Rubric_ID;
-
-    db.query(sql, (err, result) => {
-      if (err) return res.status(400).json(err);
-      else {
-        if (result.length < 1) {
-          return res.status(400).json({ error: "Rubric Row Not Assigned" });
-        }
-        //sql to check if a studet already has score
-        sql =
-          "SELECT * FROM STUDENTS_RUBRIC_ROWS_GRADE WHERE Student_ID=" +
-          Student_ID +
-          " AND Evaluator_Email=" +
-          email +
-          " AND Rubric_Row_ID=" +
-          Rubric_Row_ID;
-
-        db.query(sql, (err, result) => {
-          if (err) return res.status(400).json(err);
-
-          //if data already exists, procceed with updating
-          if (result.length > 0) {
-            sql =
-              "UPDATE STUDENTS_RUBRIC_ROWS_GRADE SET Score=" +
-              score +
-              " WHERE Student_ID = " +
-              Student_ID +
-              " AND Evaluator_Email=" +
-              email +
-              " AND Rubric_Row_ID=" +
-              Rubric_Row_ID;
-
-            db.query(sql, (err, result) => {
-              if (err) return res.status(400).json(err);
-              else {
-                if (result.length < 1) {
-                  return res.status(404).json({ error: "Rubric Not Found" });
-                }
-
-                //get the average grade of the student in that rubric and the measureID
-                sql =
-                  "SELECT Rubric_Measure_ID, AVG(Score) AS Average FROM RUBRIC_ROW NATURAL JOIN RUBRIC NATURAL JOIN STUDENTS_RUBRIC_ROWS_GRADE NATURAL JOIN RUBRIC_STUDENTS WHERE Rubric_ID=" +
-                  Rubric_ID +
-                  " AND Student_ID=" +
-                  Student_ID +
-                  " AND Rubric_ID=" +
-                  Rubric_ID;
-
-                db.query(sql, (err, result) => {
-                  if (err) return res.status(400).json(err);
-                  else {
-                    let Average_Student_Score = result[0].Average;
-                    let Rubric_Measure_ID = result[0].Rubric_Measure_ID;
-
-                    //sql to get average score of a measure
-                    sql =
-                      "UPDATE RUBRIC_STUDENTS SET Student_Avg_Grade=" +
-                      Average_Student_Score +
-                      " WHERE Rubric_Measure_ID =" +
-                      Rubric_Measure_ID;
-
-                    db.query(sql, (err, result) => {
-                      if (err) return res.status(400).json(err);
-                      else {
-                        calculateMeasure(Rubric_Measure_ID);
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          } else {
-            sql =
-              "INSERT INTO STUDENTS_RUBRIC_ROWS_GRADE (Student_ID,Evaluator_Email,Rubric_Row_ID,Score) VALUES(" +
-              Student_ID +
-              "," +
-              email +
-              "," +
-              Rubric_Row_ID +
-              "," +
-              score +
-              ")";
-
-            db.query(sql, (err, result) => {
-              if (err) return res.status(400).json(err);
-              else {
-                if (result.length < 1) {
-                  return res.status(404).json({ error: "Rubric Not Found" });
-                }
-
-                //get the average grade of the student in that rubric and the measureID
-                sql =
-                  "SELECT Rubric_Measure_ID, AVG(Score) AS Average FROM RUBRIC_ROW NATURAL JOIN RUBRIC NATURAL JOIN STUDENTS_RUBRIC_ROWS_GRADE NATURAL JOIN RUBRIC_STUDENTS WHERE Rubric_ID=" +
-                  Rubric_ID +
-                  " AND Student_ID=" +
-                  Student_ID;
-
-                db.query(sql, (err, result) => {
-                  if (err) return res.status(400).json(err);
-                  else {
-                    let Average_Student_Score = result[0].Average;
-                    let Rubric_Measure_ID = result[0].Rubric_Measure_ID;
-
-                    //sql to get average score of a measure
-                    sql =
-                      "UPDATE RUBRIC_STUDENTS SET Student_Avg_Grade=" +
-                      Average_Student_Score +
-                      " WHERE Rubric_Measure_ID =" +
-                      Rubric_Measure_ID;
-
-                    db.query(sql, (err, result) => {
-                      if (err) return res.status(400).json(err);
-                      else {
-                        calculateMeasure(Rubric_Measure_ID);
-                        return res.status(200).json({
-                          message: "Successfully updated"
-                        });
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
-    });
   }
 );
 
