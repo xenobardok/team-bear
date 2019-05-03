08edb23e0ddcaaccbd7490c0427ff2e98bde4027
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-05-03 09:48:00 -0500

    removed clutters

--- Saransh/Maximilian/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import Notifications from '../Notifications';
-
-const AppWrapper = styled.div'
-    display: flex;
-    justify-
--- Saransh/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://saransh77:xaranxh77@cluster0-1f5op.mongodb.net/test?retryWrites=true"
-};
